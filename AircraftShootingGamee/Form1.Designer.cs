d7a766ad2393449587e736f4060058fa7a97f5ee
yousef alattal <yousefmohnad984@gmail.com>
2024-12-27 22:06:00 +0300

    Add project files.

---
+++ AircraftShootingGamee/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace AircraftShootingGamee
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblGameOver = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.enemyTimer = new System.Windows.Forms.Timer(this.components);
+            this.enemyAircraft1 = new System.Windows.Forms.PictureBox();
+            this.playerAircraft1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.enemyAircraft1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerAircraft1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnRestart.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestart.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(35)))), ((int)(((byte)(41)))));
+            this.btnRestart.Location = new System.Drawing.Point(139, 221);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(150, 70);
+            this.btnRestart.TabIndex = 0;
+            this.btnRestart.Text = "Restart";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            this.btnRestart.Visible = false;
+            this.btnRestart.Click += new System.EventHandler(this.BtnRestart_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.lblScore.Location = new System.Drawing.Point(10, 10);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(88, 24);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Score: 0";
+            // 
+            // lblGameOver
+            // 
+            this.lblGameOver.AutoSize = true;
+            this.lblGameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGameOver.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.lblGameOver.Location = new System.Drawing.Point(35, 183);
+            this.lblGameOver.Name = "lblGameOver";
+            this.lblGameOver.Size = new System.Drawing.Size(344, 25);
+            this.lblGameOver.TabIndex = 2;
+            this.lblGameOver.Text = "Game Over!  Press R to Restart";
+            this.lblGameOver.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblGameOver.Visible = false;
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // enemyTimer
+            // 
+            this.enemyTimer.Interval = 2000;
+            this.enemyTimer.Tick += new System.EventHandler(this.EnemyTimer_Tick);
+            // 
+            // enemyAircraft1
+            // 
+            this.enemyAircraft1.Image = global::AircraftShootingGamee.Properties.Resources.enemyAircraft;
+            this.enemyAircraft1.Location = new System.Drawing.Point(186, 12);
+            this.enemyAircraft1.Name = "enemyAircraft1";
+            this.enemyAircraft1.Size = new System.Drawing.Size(50, 50);
+            this.enemyAircraft1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.enemyAircraft1.TabIndex = 4;
+            this.enemyAircraft1.TabStop = false;
+            this.enemyAircraft1.Visible = false;
+            this.enemyAircraft1.Click += new System.EventHandler(this.enemyAircraft1_Click);
+            // 
+            // playerAircraft1
+            // 
+            this.playerAircraft1.Image = global::AircraftShootingGamee.Properties.Resources.playerAircraft;
+            this.playerAircraft1.Location = new System.Drawing.Point(186, 378);
+            this.playerAircraft1.Name = "playerAircraft1";
+            this.playerAircraft1.Size = new System.Drawing.Size(60, 60);
+            this.playerAircraft1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.playerAircraft1.TabIndex = 3;
+            this.playerAircraft1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(35)))), ((int)(((byte)(41)))));
+            this.ClientSize = new System.Drawing.Size(415, 460);
+            this.Controls.Add(this.enemyAircraft1);
+            this.Controls.Add(this.playerAircraft1);
+            this.Controls.Add(this.lblGameOver);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.btnRestart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.enemyAircraft1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerAircraft1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblGameOver;
+        private System.Windows.Forms.PictureBox playerAircraft1;
+        private System.Windows.Forms.PictureBox enemyAircraft1;
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Timer enemyTimer;
+    }
+}
+
---
+++ AircraftShootingGamee/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace AircraftShootingGamee
+{
+    public partial class Form1 : Form
+    {
+        private Timer GameTimer;
+        private Timer EnemyTimer;
+        private Label LblScore;
+        private Label LblGameOver;
+        private Button BtnRestart;
+        private List<PictureBox> enemyList = new List<PictureBox>();
+        private List<PictureBox> projectileList = new List<PictureBox>();
+        private Random random = new Random();
+        private int score = 0;
+        private int highScore = 0;
+        private int enemiesHitGroundCount = 0; // Counter for enemies that reach the ground
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGame();
+        }
+
+        private void InitializeGame()
+        {
+            // Initialize Timers
+            GameTimer = new Timer();
+            GameTimer.Interval = 20;  // Set interval for game timer
+            GameTimer.Tick += GameTimer_Tick;
+
+            EnemyTimer = new Timer();
+            EnemyTimer.Interval = 1000;  // Set interval for enemy spawn
+            EnemyTimer.Tick += EnemyTimer_Tick;
+
+            // Load high score
+            LoadHighScore();
+
+            // Start the game and enemy timers
+            GameTimer.Start();
+            EnemyTimer.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialize additional settings if needed
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Handle player movement and shooting
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                case Keys.A:
+                    if (playerAircraft1.Left > 0) playerAircraft1.Left -= 10; // Prevent moving out of bounds
+                    break;
+                case Keys.Right:
+                case Keys.D:
+                    if (playerAircraft1.Left + playerAircraft1.Width < this.ClientSize.Width)
+                        playerAircraft1.Left += 10; // Prevent moving out of bounds
+                    break;
+                case Keys.Up:
+                case Keys.W:
+                    if (playerAircraft1.Top > 0) playerAircraft1.Top -= 10; // Prevent moving out of bounds
+                    break;
+                case Keys.Down:
+                case Keys.S:
+                    if (playerAircraft1.Top + playerAircraft1.Height < this.ClientSize.Height)
+                        playerAircraft1.Top += 10; // Prevent moving out of bounds
+                    break;
+                case Keys.Space:
+                    ShootProjectile(); // Shoot projectile when space is pressed
+                    break;
+                case Keys.R:
+                    RestartGame(); // Restart the game when R is pressed
+                    break;
+            }
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            MoveProjectiles();
+            CheckCollisions();
+            CheckGameOver();
+        }
+
+        private void EnemyTimer_Tick(object sender, EventArgs e)
+        {
+            SpawnEnemy();
+        }
+
+        private void SpawnEnemy()
+        {
+            // Spawn a new enemy aircraft at a random X position at the top of the form
+            PictureBox enemy = new PictureBox
+            {
+                Image = Properties.Resources.enemyAircraft,
+                SizeMode = PictureBoxSizeMode.StretchImage,
+                Size = new Size(50, 50),
+                Location = new Point(random.Next(this.ClientSize.Width - 50), 0)
+            };
+            this.Controls.Add(enemy);
+            enemyList.Add(enemy);
+        }
+
+        private void ShootProjectile()
+        {
+            // Create and shoot a bullet (red rectangle) from the player's aircraft position
+            PictureBox projectile = new PictureBox
+            {
+                Size = new Size(5, 20),
+                Location = new Point(playerAircraft1.Left + playerAircraft1.Width / 2 - 2, playerAircraft1.Top - 20),
+                BackColor = Color.Red
+            };
+            this.Controls.Add(projectile);
+            projectileList.Add(projectile);
+        }
+
+        private void MoveProjectiles()
+        {
+            // Move all projectiles upwards
+            foreach (var projectile in projectileList.ToList())
+            {
+                projectile.Top -= 10;
+                if (projectile.Top < 0) // Remove projectile if it goes off-screen
+                {
+                    this.Controls.Remove(projectile);
+                    projectileList.Remove(projectile);
+                }
+            }
+
+            // Move enemies downwards
+            foreach (var enemy in enemyList.ToList())
+            {
+                enemy.Top += 5;
+                if (enemy.Top >= this.ClientSize.Height) // If an enemy goes off-screen
+                {
+                    // Increment the counter for enemies that hit the ground
+                    enemiesHitGroundCount++;
+
+                    // Remove the enemy that hit the ground
+                    this.Controls.Remove(enemy);
+                    enemyList.Remove(enemy);
+                }
+            }
+        }
+
+        private void CheckCollisions()
+        {
+            // Check for projectile collisions with enemies
+            foreach (var projectile in projectileList.ToList())
+            {
+                foreach (var enemy in enemyList.ToList())
+                {
+                    if (projectile.Bounds.IntersectsWith(enemy.Bounds))
+                    {
+                        // Remove enemy and projectile on collision
+                        this.Controls.Remove(enemy);
+                        this.Controls.Remove(projectile);
+                        enemyList.Remove(enemy);
+                        projectileList.Remove(projectile);
+                        score++;
+                        lblScore.Text = "Score: " + score;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void CheckGameOver()
+        {
+            // Check if 15 enemies have hit the ground
+            if (enemiesHitGroundCount >= 15)
+            {
+                GameOver();
+            }
+        }
+
+        private void GameOver()
+        {
+            GameTimer.Stop();
+            EnemyTimer.Stop();
+            lblGameOver.Visible = true;
+            btnRestart.Visible = true;
+            SaveHighScore();
+        }
+
+        private void BtnRestart_Click(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void RestartGame()
+        {
+            // Reset all necessary variables and objects
+            score = 0;
+            enemiesHitGroundCount = 0;
+            lblScore.Text = "Score: 0\nHigh Score: " + highScore;
+
+            // Clear projectiles and enemies
+            foreach (var enemy in enemyList.ToList())
+            {
+                this.Controls.Remove(enemy);
+            }
+            enemyList.Clear();
+
+            foreach (var projectile in projectileList.ToList())
+            {
+                this.Controls.Remove(projectile);
+            }
+            projectileList.Clear();
+
+            // Restart the game and enemy timers
+            GameTimer.Start();
+            EnemyTimer.Start();
+            lblGameOver.Visible = false;
+            btnRestart.Visible = false;
+        }
+
+        private void SaveHighScore()
+        {
+            if (score > highScore)
+            {
+                highScore = score;
+                File.WriteAllText("highscore.txt", highScore.ToString());
+            }
+        }
+
+        private void LoadHighScore()
+        {
+            if (File.Exists("highscore.txt"))
+            {
+                highScore = int.Parse(File.ReadAllText("highscore.txt"));
+            }
+            lblScore.Text = "Score: 0\nHigh Score: " + highScore;
+        }
+
+        // You can leave these click event handlers empty if they are not used:
+        private void pictureBox2_Click(object sender, EventArgs e) { }
+        private void enemyAircraft1_Click(object sender, EventArgs e) { }
+    }
+}
